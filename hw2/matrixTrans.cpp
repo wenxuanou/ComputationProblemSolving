b84a3b54a986fac01ade69c0ab37ae70118d7ad8
Ubuntu <ubuntu@ip-172-31-25-192.us-east-2.compute.internal>
2021-03-09 04:31:46 +0000

    changed matrix datatype, finished timing

--- hw2/matrixTrans.cpp
+++ hw2/matrixTrans.cpp
@@ -1,84 +1,78 @@
 #include<iostream>
-#include<stdio.h>
+#include<iomanip>
+#include<sys/time.h>
 #include<cmath>
 #include<time.h>
 
 using namespace std;
 
-double* genMatrix(int dim, int rank);
+unsigned char* genMatrix(int dim, int rank);
 
 double* transpose2(double mat[], int dim);
-double* transpose(double mat[], int dim, int rank);
+unsigned char* transpose(unsigned char mat[], int dim, int rank);
 
 
-void print2D(double mat[], int dim);
 void printMat(double mat[], int dim, int rank);
-void printStraight(double mat[], int dim, int rank);
+void printStraight(unsigned char mat[], int dim, int rank);
+
 
 int main(){
 	cout << "run" << endl;
 
-	int dim = 2, rank = 3; // rank >= 2
-	
+	// set up timing
+	struct timeval start, end;
+
+	int dim = 2, rank = 23; // rank >= 2	
 	// initialize matrix
-	double* mat = genMatrix(dim, rank);
+	unsigned char* mat = genMatrix(dim, rank);
 
-	//double* matTrans = transpose2(mat,dim);
-	double* matTrans2 = transpose(mat, dim, rank);
+	gettimeofday(&start, NULL);
 
+	unsigned char* matTrans2 = transpose(mat, dim, rank);
 	
+	gettimeofday(&end, NULL);
+
+	// runtime
+	double time_taken;
+	time_taken = (end.tv_sec - start.tv_sec) * 1e6; 
+	time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
+	cout << std::setprecision(6) << "runtime: " << time_taken << " sec" << endl;
+
+
+	// print matrix
+	/*
 	cout << "matrix:" << endl;
 	//printMat(mat, dim, rank);
 	printStraight(mat, dim, rank);
-	
 
-	/*	
 	cout << "transpose:" << endl;
-        print2D(matTrans, dim);	
-	*/
-
-	cout << "new transpose:" << endl;
 	//printMat(matTrans2, dim, rank);	
 	printStraight(matTrans2, dim, rank);
-
+	*/
 
 
 
 	return 0;
 }
 
-double* genMatrix(int dim, int rank){
+unsigned char* genMatrix(int dim, int rank){
 	// generate vectorized tensor
 	const int matSize = pow(dim, rank); // square tensor
-	double* matrix = new double[matSize];
+	unsigned char* matrix = new unsigned char[matSize];
 	srand (time(NULL));
 	for(int id = 0; id < matSize; id++){
-		//matrix[id] = rand() % 10; 
-		matrix[id] = id + 1;
+		matrix[id] = rand() % 10; 
+		//matrix[id] = id + 1;
 	}
 
 	return matrix;
 }
 
 
-double* transpose2(double mat[], int dim){
-	// for rank 2 only
-	const int matSize = dim * dim;
-	double* out = new double [matSize];
-
-	for(int id = 0; id < matSize; id++){
-		int i = id / dim;
-		int j = id % dim;
-		out[j * dim + i] = mat[id];
-	}	
-
-	return out;
-}
-
-double* transpose(double mat[], int dim, int rank){
+unsigned char* transpose(unsigned char mat[], int dim, int rank){
 	// all tensor are stored as 1D vector, square tensor
 	const int matSize = pow(dim,rank);
-	double* out = new double[matSize];	
+	unsigned char* out = new unsigned char[matSize];	
 
 	for(int id = 0; id < matSize; id++){
 		int newId = 0;

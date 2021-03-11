4a024ede431e4b32a65c8a7bb7231a2ac37ebc70
wenxuanou <wou@andrew.cmu.edu>
2021-03-11 14:37:24 -0500

    final version, finished measurement

--- hw2/matrixTrans.cpp
+++ hw2/matrixTrans.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 #include<iomanip>
 #include<sys/time.h>
 #include<cstdlib>
@@ -12,7 +13,7 @@
 
 using namespace std;
 
-void genMatrix(char* mat, unsigned long dim, int rank);
+void genMatrix(char* mat, unsigned long matSize);
 
 void transpose(char* mat, unsigned long dim, int rank);
 
@@ -23,24 +24,33 @@ void printStraight(char* mat, unsigned long dim, int rank);
 int main(){
 	cout << "run" << endl;
 	cout << "core used: " << NUM_THREADS << endl;
+	
+	// write to file
+	ofstream outfile("runtimelog.txt");
+	outfile << "core used: " << NUM_THREADS << endl;
 
 	// set up timing
 	struct timeval start, end;
 
 	//int rank = 2; 
-	const int power = 32;
+	const int power = 28;
+	int maxRank = 28;
 	const unsigned long matSize = pow(2,power); // 4GB size. 2^32
 	char* mat = (char*) malloc(matSize);
-	for(int rank = 2; rank <= power; rank *= 4 ){	// *= 4
+	cout << "matSize: " << matSize << endl;
+	outfile << "matSize: 2^" << power << endl;
+
+	// initialize tensor as 1D array
+	genMatrix(mat, matSize);
+	
+	for(int rank = 2; rank <= maxRank; rank += 2 ){	// *= 4
 
 		cout << "rank: " << rank << " ";
+		outfile << "rank: " << rank << " ";
 
 		// compute matrix dimension
 		double root = 1.0 / rank;
 		unsigned long dim = floor(pow(matSize,root));	
-
-		// initialize matrix
-		genMatrix(mat, dim, rank);
 
 		/*
 		cout << endl;
@@ -60,7 +70,7 @@ int main(){
 		time_taken = (end.tv_sec - start.tv_sec) * 1e6; 
 		time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
 		cout << std::setprecision(6) << "runtime: " << time_taken << " sec" << endl;
-
+		outfile << std::setprecision(6) << "runtime: " << time_taken << " sec" << endl;
 
 	// print matrix
 	/*	
@@ -77,15 +87,16 @@ int main(){
 	}
 
 	free(mat);
+	outfile.close();	
 
 	return 0;
 }
 
-void genMatrix(char* matrix, unsigned long dim, int rank){
+void genMatrix(char* matrix, unsigned long matSize){
 	// generate vectorized tensor
-	const unsigned long matSize = pow(dim, rank); // square tensor
-
 	srand (time(NULL));
+
+	#pragma omp parallel for num_threads(NUM_THREADS) schedule(dynamic) 
 	for(unsigned long id = 0; id < matSize; id++){
 		matrix[id] = rand() % 10 + '0'; 
 		//matrix[id] = id + 1;
@@ -100,10 +111,11 @@ void transpose(char* mat, unsigned long dim, int rank){
 	const unsigned long matSize = pow(dim,rank);
 	//unsigned char* out = new unsigned char[matSize];	
 	
-	#pragma omp parallel for num_threads(NUM_THREADS)	
-	for(unsigned long id = 0; id < matSize; id ++){
-		unsigned long newId = 0;
-		unsigned long temp = id;
+	unsigned long newId, temp;
+	#pragma omp parallel for num_threads(NUM_THREADS) private(newId, temp) schedule(guided)
+	for(unsigned long id = 0; id < matSize; id++){
+		newId = 0;
+		temp = id;
 		for(int countR = 1; countR <= rank; countR++){
 			newId += temp / (unsigned long)pow(dim, rank - countR) * pow(dim, countR - 1); 	
 			temp = temp % (unsigned long)pow(dim, rank - countR);
